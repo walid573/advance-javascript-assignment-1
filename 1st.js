609bb478b5f97980161e2253c22256f722b9ea52
walid573 <mdwalid19833@gmail.com>
2024-07-15 23:59:05 +0600

    important change

--- 1st.js
+++ 1st.js
@@ -17,8 +17,8 @@ function scopeTest() {
 
 //2
   function greet(name) {
-    console.log("Hello, " + name + "!");
-    console.log("Good morning," + name)
+    console.log("Hello, " + name + "!"); //Hello, Alice!
+    console.log("Good morning," + name)//Good morning,Bob
   }
 greet("Alice")
 
@@ -31,10 +31,10 @@ const sum = (function() {
     return args.reduce((a, b) => a + b, 0);
     };
     })();
-    console.log(sum(0,1,2)); 
-    console.log(sum(1,2,3,4));
-    console.log(sum(5));
-    console.log(sum(0));
+    console.log(sum(1,2,3)); // 6
+    console.log(sum(10,20,30,40)); //100
+    console.log(sum(5)); //5
+    
 //4
 
 
@@ -44,7 +44,9 @@ const array3 = [7,8,9];
 
 const mergedArray = [...array1,...array2,...array3];
 
-console.log(mergedArray)
+console.log(mergedArray) //[1,2,3,4,5,6,7,8,9]
+    
+
 
 //5
 const name1 = "John";
@@ -52,7 +54,7 @@ const age1 = 30;
 
 const greeting = `Hello, I'm ${name1} and I am ${age1} years old.`;
 
-console.log(greeting);
+console.log(greeting); //Hello, I'm John and I am 30 years old.
 
 
 //6 
@@ -60,7 +62,7 @@ console.log(greeting);
 const fruits = ["apple", "banana", "cherry"];
 
 for (const fruit of fruits) {
-    console.log(fruit);
+    console.log(fruit); //apple banana cherry
 }
 
 //7
@@ -70,7 +72,7 @@ const age2 = 25;
 
 const greeting1 = `My name is ${name2} and I am ${age2} years old.`;
 
-console.log(greeting1);
+console.log(greeting1); //My name is Alice and I am 25 years old.
 
 
 // 8 
@@ -82,7 +84,7 @@ const students = [
   ];
   
   for (const grades of students ) {
-    console.log(grades);
+    console.log(grades);  //{ name: 'Alice', grade: 90 } { name: 'Bob', grade: 85 } { name: 'Charlie', grade: 92 }
 }
 
 
@@ -101,7 +103,7 @@ for(let i=0;i<nestedArrays.length;i++){
   }
 }
 
-console.log(sum1);
+console.log(sum1); //45
 
 
 //10
@@ -110,7 +112,7 @@ const b = 10;
 
 const result = `The sum of ${a} and ${b} is ${a + b}.`;
 
-console.log(result);
+console.log(result); //The sum of 5 and 10 is 15.
 
 
 
